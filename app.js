9c3ba9e6d94051f00fa55fd9be94f933b904492c
Houcine <houcinehannoun@gmail.com>
2024-11-22 16:17:25 +0100

    add alerts to the UI

--- app.js
+++ app.js
@@ -40,20 +40,30 @@ class UI {
             <td>${book.isbn}</td>
             <td><a href='#' class='btn btn-danger btn-sm delete' >X</a></td>
          `;
-
     list.appendChild(row);
-  };
+  }
 
   static deleteBook(element) {
-    if (element.classList.contains('delete')) {
+    if (element.classList.contains("delete")) {
       element.parentElement.parentElement.remove();
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const formWrapper = document.querySelector('.container .d-flex');
+    container.insertBefore(div, formWrapper);
+
+    setTimeout(() => div.remove(), 4000);
+  }
+
   static clearFields() {
-    document.querySelector("#title").value='';
-    document.querySelector("#author").value='';
-    document.querySelector("#isbn").value='';
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
   }
 }
 
@@ -66,19 +76,27 @@ document.addEventListener("DOMContentLoaded", UI.displayBooks);
 // Event to add a book
 
 document.querySelector("#book-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    // Get form Values
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const isbn = document.querySelector('#isbn').value;
+  e.preventDefault();
+  // Get form Values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
 
-    // Create a book 
+  // Check form before submission
+
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    // Create a book
     const book = new Book(title, author, isbn);
     UI.addBookToList(book);
+    UI.showAlert('Book Added !', 'success')
     UI.clearFields();
+  }
 });
 
 // Event to remove a book
-document.querySelector('#book-list').addEventListener('click', (e) => {
+document.querySelector("#book-list").addEventListener("click", (e) => {
   UI.deleteBook(e.target);
+  UI.showAlert('Book removed !', 'info')
 });
